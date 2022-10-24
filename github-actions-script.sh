c664e326de6155d10829f46f5888907fc59b09ee
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-10-24 12:31:38 +0200

    run shell commands

---
+++ github-actions-script.sh
@@ -0,0 +1,13 @@
+      echo what is in this directory?
+      ls -a
+      echo
+      echo is node Installed?
+      node --version
+      echo
+      echo is Git Installed?
+      git --version
+      echo
+      echo What about build tools?
+      mvn --version
+      gradle --version
+      ant -version
